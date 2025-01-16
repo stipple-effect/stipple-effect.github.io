3d75ee23b065018ba61a1d28113aefe2611b5e7a
Jordan Bunke <jordantb99@gmail.com>
2025-01-16 11:24:37 +0000

    Updated DeltaScript parsing algorithm for accurate syntax highlighting

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -6,25 +6,36 @@ window.onload = () => {
 };
 
 function parseDeltascript() {
+    const replace = new Set([
+        ".nx", ".nl"
+    ]);
+
+    const unrecKeywords = new Set([
+        "when", "is", "matches", "passes", "otherwise", "new", "in", "final"
+    ]);
     const dsTypes = new Set([
         "int", "bool", "float", "char", "string", "color", "image", 
         "project", "layer", "palette", "script", "save_config", "light"
     ]);
+
+    replace.forEach(r => {
+        document.querySelectorAll(r).forEach(codeWord => {
+            const next = codeWord.nextElementSibling;
+            const previous = codeWord.previousElementSibling;
     
-    document.querySelectorAll(".nx").forEach(codeWord => {
-        const next = codeWord.nextElementSibling;
-        const previous = codeWord.previousElementSibling;
-
-        if (dsTypes.has(codeWord.innerHTML))
-            codeWord.setAttribute("class", "ds-type");
-        else if (codeWord.innerHTML.startsWith("$"))
-            codeWord.setAttribute("class", "namespace");
-        else if (next instanceof Element && next.innerHTML.startsWith("("))
-            codeWord.setAttribute("class", "ds-function");
-        else if (previous instanceof Element && previous.innerHTML === ".")
-            codeWord.setAttribute("class", "ds-property");
+            if (unrecKeywords.has(codeWord.innerHTML))
+                codeWord.setAttribute("class", "k");
+            else if (dsTypes.has(codeWord.innerHTML))
+                codeWord.setAttribute("class", "ds-type");
+            else if (codeWord.innerHTML.startsWith("$"))
+                codeWord.setAttribute("class", "namespace");
+            else if (next instanceof Element && next.innerHTML.startsWith("("))
+                codeWord.setAttribute("class", "ds-function");
+            else if (previous instanceof Element && previous.innerHTML === ".")
+                codeWord.setAttribute("class", "ds-property");
+        });
     });
-
+    
     document.querySelectorAll(".err").forEach(codeWord => {
         const next = codeWord.nextElementSibling;
 
